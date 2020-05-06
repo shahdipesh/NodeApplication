c0f0413a9b06c904cd73dea8ff3583742263b6fa
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-05 22:31:05 -0500

    create user api

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.get("/", function (req, res) {
   });
 });
 
+app.post("/create/user", function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query(`INSERT INTO facebook (username,password) VALUES ("${username}","${password}") `, function (err, rows) {
+        if (err)throw err;
+      res.send(rows);
+    });
+  });
+
+
+
 app.listen(3000, function () {
   console.log("Server started at port 3000");
 });
