5632e937296ab93415feef444c1fd3837235ede3
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-06 11:40:02 -0500

    adding mysql connection feature

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require ('express');
 
+var pool = require('./connection');
+
 var app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Sever running successfully");
+    pool.query(
+        `SELECT * FROM facebook`,(err,rows)=>{
+            if (err)throw err;
+            res.send(rows);
+        }
+    )
 })
 app.listen (3000,()=>{
     console.log ("Server started at port 3000");
