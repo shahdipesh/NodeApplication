270790e933aa2f4e1fd0d754d7c8a1088ea033fa
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-05 23:19:47 -0500

    setting of env file

--- connection.js
+++ connection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
- 
+require('dotenv').config();
+
+console.log('URL, USER, PASSWORD ',process.env.URL, process.env.DB_USERNAME, process.env.DB_PASSWORD);
+
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password:'root',
-  database: 'test',
+  host: process.env.URL,
+  user: process.env.DB_USERNAME,
+  password:process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+
 var pool = require("./connection");
 
 var app = express();
