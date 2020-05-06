95497891b0a2ceb7435b9e1e537ae5624d2db379
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-06 12:08:16 -0500

    handling CRUD requests

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var express = require ('express');
 var pool = require('./connection');
 
 var app=express();
+app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/alluser',(req,res)=>{
     pool.query(
         `SELECT * FROM facebook`,(err,rows)=>{
             if (err)throw err;
@@ -12,6 +13,39 @@ app.get('/',(req,res)=>{
         }
     )
 })
+
+app.post('/createUser',(req,res)=>{
+    var username = req.body.username;
+    var password= req.body.password;
+    pool.query(
+        `INSERT INTO facebook (username,password) VALUES (?,?);`,[username,password],(err,rows)=>{
+            if (err)throw err;
+            res.send(rows);
+        }
+    )
+})
+
+app.delete('/deleteUser',(req,res)=>{
+    var username = req.body.username;
+    pool.query(
+        `DELETE FROM facebook WHERE username =?;`,[username],(err,rows)=>{
+            if (err)throw err;
+            res.send(rows);
+        }
+    )
+})
+
+app.put('/updateUser',(req,res)=>{
+    var username = req.body.username;
+    var oldUsername = req.body.oldUsername;
+    pool.query(
+        `UPDATE facebook SET username =? WHERE username =?;`,[username,oldUsername],(err,rows)=>{
+            if (err)throw err;
+            res.send(rows);
+        }
+    )
+})
+
 app.listen (3000,()=>{
     console.log ("Server started at port 3000");
 })
